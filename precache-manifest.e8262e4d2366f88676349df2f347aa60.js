e2d2ae57d641e88352819b79b20b718a0934222a
Igor Tyshchenko <igor@tyshchenko.me>
2020-02-25 13:55:45 +1100

    Updates

---
+++ precache-manifest.e8262e4d2366f88676349df2f347aa60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e88886d381328247cff4fef6dbb03eb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3f6bec0a70109a5924e",
+    "url": "/static/css/main.cd1558e4.chunk.css"
+  },
+  {
+    "revision": "c11b44bec88a735ae963",
+    "url": "/static/js/2.2d3b026f.chunk.js"
+  },
+  {
+    "revision": "b9a8ac0abc0a7a1a1cda97b73c23bd3c",
+    "url": "/static/js/2.2d3b026f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3f6bec0a70109a5924e",
+    "url": "/static/js/main.8cee8038.chunk.js"
+  },
+  {
+    "revision": "2cf8ff73638a0b5339af",
+    "url": "/static/js/runtime-main.bf4726b6.js"
+  }
+]);
