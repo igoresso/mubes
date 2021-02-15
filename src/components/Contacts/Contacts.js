7710122fcf3660137f1e7a3f18f33dad05a857de
Igor Tyshchenko <igor@tyshchenko.me>
2021-02-15 17:44:56 +1100

    Updated Contacts page

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -1,49 +1,10 @@
-import React, { useState } from 'react';
-import { Formik } from 'formik';
-import * as Yup from 'yup';
+import React from 'react';
 import { Helmet } from "react-helmet";
 import { Container, Form, Row, Col, Button, Spinner, Alert } from 'react-bootstrap';
 
-const schema = Yup.object({
-  name: Yup.string().required("Please enter your name"),
-  email: Yup.string().email("Please enter a valid email").required("Please enter your email"),
-  subject: Yup.string().required("Please enter a subject"),
-  message: Yup.string().required("Please enter your message"),
-  recaptcha: Yup.string().required()
-});
+import './Contacts.scss';
 
 export default () => {
-  const [showAlert, setShowAlert] = useState(false);
-  const [alertVariant, setAlertVariant] = useState("");
-  const [alertMessage, setAlertMessage] = useState("");
-
-  const handleSubmit = (values, actions) => {
-    const formData = new FormData();
-    formData.set('name', values.name);
-    formData.set('email', values.email);
-    formData.set('subject', values.subject);
-    formData.set('message', values.message);
-    formData.set('recaptcha', values.recaptcha);
-
-    const url = "";
-    fetch(url, {
-      method: 'POST',
-      body: formData
-    })
-    .then(response => {
-      actions.resetForm();
-      setAlertVariant("success");
-      setAlertMessage("Thank you for contacting us. We will get back to you soon!");
-      setShowAlert(true);
-    })
-    .catch(error => {
-      actions.setSubmitting(false)
-      setAlertVariant("danger");
-      setAlertMessage("Oh, no! Something is wrong.");
-      setShowAlert(true);
-    })
-  }
-
   return (
     <Container as="section">
       <Helmet>
@@ -52,125 +13,28 @@ export default () => {
       </Helmet>
 
       <h1 className="page-title mb-5 pt-2 text-center">Contacts</h1>
-      <p className="text-center">Do you have any questions? Please do not hesitate to contact us directly or fill in the form below. Email:&#160;<a href="mailto:info@mubes.club">mubes.unimelb@gmail.com</a></p>
-      <Row className="justify-content-md-center">
-        <Col lg={6} xl={5}>
-          <Formik
-          initialValues={{
-            name: "",
-            email: "",
-            subject: "",
-            message: "",
-            recaptcha: ""
-          }}
-          validationSchema={ schema }
-          onSubmit={ handleSubmit }
-          validateOnMount
-        >
-          {({
-            values,
-            errors,
-            handleChange,
-            handleBlur,
-            handleSubmit,
-            isSubmitting,
-            setFieldValue,
-            submitCount
-          }) => (
-            <Form className="pb-4" noValidate onSubmit={ handleSubmit }>
-              <Form.Group controlId="name">
-                <Form.Label>Name</Form.Label>
-                <Form.Control
-                  type="text"
-                  name="name"
-                  placeholder="John Snow"
-                  value={ values.name }
-                  onChange={ handleChange }
-                  onBlur={ handleBlur }
-                  isInvalid={ submitCount>0 && !!errors.name }
-                  disabled
-                />
-                <Form.Control.Feedback type="invalid">
-                  { errors.name }
-                </Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group controlId="email">
-                <Form.Label>Email</Form.Label>
-                <Form.Control
-                  type="email"
-                  name="email"
-                  placeholder="your@email.com"
-                  value={ values.email }
-                  onChange={ handleChange }
-                  onBlur={ handleBlur }
-                  isInvalid={ submitCount>0 && !!errors.email }
-                  disabled
-                />
-                <Form.Control.Feedback type="invalid">
-                  { errors.email }
-                </Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group controlId="subject">
-                <Form.Label>Subject</Form.Label>
-                <Form.Control
-                  type="text"
-                  name="subject"
-                  value={ values.subject }
-                  onChange={ handleChange }
-                  onBlur={ handleBlur }
-                  isInvalid={ submitCount>0 && !!errors.subject }
-                  disabled
-                />
-                <Form.Control.Feedback type="invalid">
-                  { errors.subject }
-                </Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group controlId="message">
-                <Form.Label>Message</Form.Label>
-                <Form.Control
-                  as="textarea" 
-                  rows="5"
-                  name="message"
-                  value={ values.message }
-                  onChange={ handleChange }
-                  onBlur={ handleBlur }
-                  isInvalid={ submitCount>0 && !!errors.message }
-                  disabled
-                />
-                <Form.Control.Feedback type="invalid">
-                  { errors.message }
-                </Form.Control.Feedback>
-              </Form.Group>
-              {/*
-              <Recaptcha
-                sitekey="6Ldeb9wUAAAAAKDaIIz8AObKkIvDtEY8R4XtvVTW"
-                render="explicit"
-                onloadCallback={() => true}
-                verifyCallback={ res => setFieldValue('recaptcha', res) }
-                expiredCallback={ res => setFieldValue('recaptcha', "") }
-              />
-              */}
-              <Button type="submit" className="mt-2" disabled={ isSubmitting || errors.recaptcha }>
-                { isSubmitting &&
-                  <Spinner
-                    as="span"
-                    animation="grow"
-                    size="sm"
-                    className="mr-1"
-                    role="status"
-                    aria-hidden="true"
-                  />
-                }
-                { isSubmitting ? "Submitting..." : "Submit"}
-              </Button>
-            </Form>
-          )}
-          </Formik>
-          { showAlert &&
-            <Alert variant={ alertVariant } onClose={ () => setShowAlert(false) } dismissible>
-              { alertMessage }
-            </Alert>
-          }
+
+      <p className="lead text-center mb-4">Do you have any questions? Please do not hesitate to contact us directly.</p>
+      
+      <Row className="mb-4">
+        <Col md={5}>
+          <a href="mailto:mubes.unimelb@gmail.com" className="contacts-btn" target="_blank">
+            <img
+              src="img/gmail.svg"
+              alt="Email us"
+            />
+          </a>
+        </Col>
+        <Col md={2} className="d-flex justify-content-center align-items-center">
+          <span className="contacts-separator"></span>
+        </Col>
+        <Col md={5} className="d-flex justify-content-center">
+          <a href="https://www.messenger.com/t/officialmubespage/" className="contacts-btn" target="_blank">
+            <img
+              src="img/messenger.svg"
+              alt="Message us"
+            />
+          </a>
         </Col>
       </Row>
     </Container>
